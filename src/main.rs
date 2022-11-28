630ec466cd27fdcec310a8b39fe0afe0f5c4817b
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-28 20:58:46 +0200

    mutable vector

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,13 @@ fn main() {
     let v_two = vec![1, 2, 3];
 
     println!("This is non-empty vector! {:?}", v_two);
+
+    let mut v_mut = vec![1];
+
+    println!("This is mutable vector! {:?}", v_mut);
+
+    v_mut.push(6);
+    v_mut.push(8);
+
+    println!("This is mutable vector! {:?}", v_mut);
 }
