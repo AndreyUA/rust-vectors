a715494c5e72a031b2313376954bef86c2f11e76
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-28 20:56:55 +0200

    created vector with vec![] macros

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     let v: Vec<i32> = Vec::new();
 
     println!("This is empty vector! {:?}", v);
+
+    let v_two = vec![1, 2, 3];
+
+    println!("This is non-empty vector! {:?}", v_two);
 }
